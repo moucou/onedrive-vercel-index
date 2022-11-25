99264170703fc07ea5dff4ba22315f96454431bf
moucou <118466917+moucou@users.noreply.github.com>
2022-11-26 01:03:00 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Private', '/OneSyncFiles'],
+  protectedRoutes: ['/Private', '/OneSyncFiles', '/Homework'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:moucou@qq.com',
